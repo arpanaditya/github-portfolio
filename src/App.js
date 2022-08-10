6647c12062a7609e1dde37ee4eabdf20396b8c58
arpanaditya <adityaarpansahoo@gmail.com>
2022-08-10 10:31:03 +0530

    feat: create home page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Herosection from "./components/hero/Herosection";
+import Navbar from "./components/nav/Navbar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-    
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+      {/* <Route path="/navbar" component={Navbar} /> */}
+      <Route path="/hero" component={Herosection} />
+        {/* <Herosection /> */}
+      </div>
+    </Router>
   );
 }
 
