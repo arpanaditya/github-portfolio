e498718ce6ebdb3c0034c555fd3a200f36198844
arpanaditya <adityaarpansahoo@gmail.com>
2022-08-18 20:43:05 +0530

    feat: integrate GitHub API in hero component

--- src/components/hero/Herosection.jsx
+++ src/components/hero/Herosection.jsx
@@ -1,41 +1,37 @@
-import { SiInstagram, SiLinkedin, SiTwitter } from "react-icons/si";
-import avatar from "./../../assets/headshot-image.jpg";
+import React, { useState, useEffect } from "react";
+import { SiTwitter } from "react-icons/si";
+import { BiLink } from "react-icons/bi";
+// import avatar from "./../../assets/headshot-image.jpg";
 import "./Herosection.css";
+import { username, API_URL } from "../Constant";
+import axios from "axios";
 
 const Herosection = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios.get(`${API_URL}/users/${username}`).then((res) => setData(res.data));
+  });
+
   return (
     <div className="hero-container">
       <div className="left">
-        <img src={avatar} alt="avatar" />
+        <img src={data.avatar_url} alt="avatar" />
       </div>
       <div className="right">
-        <h1 className="heading">Aditya Arpan Sahoo</h1>
-        <p className="username">(@arpanaditya)</p>
-        <p className="userinfo">
-          Software Engineer @DigitalProductschool | @mlh mentor | @postmanlabs
-          student expert | @scaleracademy Elite
-        </p>
+        <h1 className="heading">{data.name}</h1>
+        <p className="username">(@{data.login})</p>
+        <p className="userinfo">{data.bio}</p>
         <div className="socials">
           <a
-            href="http://twitter.com/adityaa_sahoo"
+            href={`http://twitter.com/${data.twitter_username}`}
             target="_blank"
             rel="noopener noreferrer"
           >
             <SiTwitter color="black" size={35} />
           </a>
-          <a
-            href="http://instagram.com/adityaa_sahoo"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <SiInstagram color="black" size={35} />
-          </a>
-          <a
-            href="http://linkedin.com/in/arpan-aditya"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <SiLinkedin color="black" size={35} />
+          <a href={`https://${data.blog}`} target="_blank" rel="noopener noreferrer">
+            <BiLink color="black" size={35} />
           </a>
         </div>
       </div>
