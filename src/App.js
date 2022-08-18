4fb95b42ae41ceca729971743c39756a727f94fb
arpanaditya <adityaarpansahoo@gmail.com>
2022-08-18 20:42:04 +0530

    feat: add contribute component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Contributions from "./components/contributions/Contributions";
 import Herosection from "./components/hero/Herosection";
 import Navbar from "./components/nav/Navbar";
 import Projects from "./components/projects/Projects";
@@ -12,7 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Herosection />} />
           <Route path="/projects" element={<Projects />} />
-          <Route path="/contribution" element={<Herosection />} />
+          <Route path="/contribution" element={<Contributions />} />
         </Routes>
       </div>
     </Router>
