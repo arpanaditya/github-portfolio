c3d157454968e0efb80b474846fbeceb398c5296
arpanaditya <adityaarpansahoo@gmail.com>
2022-08-18 20:42:41 +0530

    fix: replace urls and usernames with constants

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from "react";
 import "./Projects.css";
 import axios from "axios";
+import { username, API_URL } from "../../components/Constant";
 
 const Projects = () => {
   const [data, setData] = useState([]);
   useEffect(() => {
     axios
-      .get("https://api.github.com/users/arpanaditya/repos")
+      .get(`${API_URL}/users/${username}/repos`)
       .then((res) => setData(res.data))
       .then((error) => console.log(error));
   }, []);
@@ -20,26 +21,18 @@ const Projects = () => {
         {data.map((item) => {
           return (
             <div className="box">
-            <a href={item.html_url} target='_blank' rel="noopener noreferrer">{item.name}</a>
-              {/* {item.name} */}
+              <a href={item.html_url} target="_blank" rel="noopener noreferrer">
+                {item.name}
+              </a>
               <p className="description">{item.description}</p>
-              <p className="lang"><span style={{'font-weight': 'bold'}}>Languages:</span> {item.language}</p>
+              <p className="lang">
+                <span style={{ "font-weight": "bold" }}>Languages:</span>{" "}
+                {item.language}
+              </p>
+              <p className="demo"><a href={item.homepage} target="_blank" rel="noopener noreferrer">Preview</a></p>
             </div>
           );
         })}
-        {/* <div className="box">
-          Project Name
-          <p className="description">
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Harum,
-            maiores!
-          </p>
-        </div> */}
-        {/* <div className="box">box2</div>
-        <div className="box">box3</div>
-        <div className="box">box4</div>
-        <div className="box">box5</div>
-        <div className="box">box6</div> */}
-        {/* <img src="http://ghchart.rshah.org/arpanaditya" alt="2016rshah's Github chart" /> */}
       </div>
     </div>
   );
